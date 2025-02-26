2bfbfb03d1ac30456d1c236b02429d84916e58e0
Jin19121 <jinkim19121@gmail.com>
2025-02-26 14:58:04 +0900

    0226 김지민 refactor: ec2 재실행

--- backend/build.sh
+++ backend/build.sh
@@ -9,5 +9,5 @@ cd ../backend
 
 ./gradlew bootJar
 
-scp -i src/main/resources/secret/key1121.pem build/libs/backend-0.0.1-SNAPSHOT.jar ubuntu@13.209.50.217:./prj.jar
-ssh -i src/main/resources/secret/key1121.pem ubuntu@13.209.50.217 './run.sh'
+scp -i src/main/resources/secret/key1121.pem build/libs/backend-0.0.1-SNAPSHOT.jar ubuntu@52.79.239.190:./prj.jar
+ssh -i src/main/resources/secret/key1121.pem ubuntu@52.79.239.190 './run.sh'
